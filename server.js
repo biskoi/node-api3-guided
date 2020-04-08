d508954f8534ef127f8c5934c5679ca656e40407
biskoi <biskoi@daydream.cafe>
2020-04-08 11:57:42 -0600

    it works

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express'); // importing a CommonJS module
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+// middleware before routers, being used globally
+server.use(logger)
+// server.use(gatekeeper('melon'))
+server.use(helmet());
+server.use(morgan('short'));
+server.use(express.json()); // built-in middleware
 
-server.use('/api/hubs', hubsRouter);
+// server.use('/api/hubs', hubsRouter);
+server.use('/api/hubs', gatekeeper('hubspw'), hubsRouter); // using MW locally - will execute gatekeeper before going to hubs router
 
 server.get('/', (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
@@ -18,3 +26,32 @@ server.get('/', (req, res) => {
 });
 
 module.exports = server;
+
+// If you want to write middleware with arrow functions, they need to be declared before the use(mw), otherwise write as normal functions
+
+// Normally MW is written in a separate file, and is then required in the server
+
+function logger(req, res, next) { 
+  console.log(req.method, req.originalUrl, 'req.query', req.query);
+  next(); // call next to pass the data on to the next mw, otherwise it stops here
+};
+
+// write and use middleware to
+// - read a 'pass' header from req.query
+// - if pw is melon, let req continue
+// - else status 400
+
+function gatekeeper(pass) { // Needs to return the actual MW function so express can use it - remember to invoke and pass in a pw arg
+
+  return function(req, res, next) { // function gatekeeper
+
+    // const {pass} = req.query;
+  
+    if (req.query.pass === pass) {
+      next();
+    } else {
+      res.status(400).json({message: 'Incorrect password'});
+    };
+  };
+
+};
